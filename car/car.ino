496ff0ac8ca84cd7a727b98c96375fffbe053794
acytoo <acytoo@gmail.com>
2023-04-02 09:31:55 -0400

    add moisture sensor

--- car/car.ino
+++ car/car.ino
@@ -1,38 +1,63 @@
 #include <IRremote.h>
-const int irReceiverPin =7;  //定义红外接收模块的信号接收为D7引脚 
-const int ledPin1 = 13;       //将D13引脚定义为双色LED绿灯的信号输出引脚
-const int ledPin2 = 12;       //将D12引脚定义为双色LED红灯的信号输出引脚
-IRrecv irrecv(irReceiverPin); //定义一个红外接收模块信号的变量
+#include <AccelStepper.h>
+
+#define MotorInterfaceType 4
+
+const int irReceiverPin = 13; // digital in
+const int moiAo = 0; // analog in
+const int moiDo = 6; // digital in 
+IRrecv irrecv(irReceiverPin);
 decode_results results;
-void setup()
-{
-  pinMode(ledPin1,OUTPUT);   //定义LED信号输出引脚
-  pinMode(ledPin2,OUTPUT);   //定义LED信号输出引脚
-  Serial.begin(9600);     //定义波特率
-  irrecv.enableIRIn();    //给红外接收模块进行使能 
+AccelStepper left(MotorInterfaceType, 2, 4, 3, 5);
+AccelStepper right(MotorInterfaceType, 8, 10, 9, 11);
+
+void setup() {
+  Serial.begin(9600); 
+  irrecv.enableIRIn();
+  
+  left.setMaxSpeed(500.0);
+  left.setAcceleration(100.0);
+  //left.setSpeed(500.0);
+  
+  right.setMaxSpeed(500.0);
+  right.setAcceleration(100.0);
+  //right.setSpeed(-500.0);
+
+  left.moveTo(10);
+  right.moveTo(-10);
 }
-void loop() 
-{
-  if (irrecv.decode(&results)) //当红外接收模块接收到信号时
-  { 
-    Serial.print("irCode: "); //打印出"irCode: " 
-    Serial.print(results.value, HEX); //打印出当前信号16进制的值
-    Serial.print(", bits: "); //打印出" , bits: " 
-    Serial.println(results.bits); //打印出比特值
-    irrecv.resume(); // 接收下一个信号
+void loop() {
+
+  //Serial.println(analogRead(moiAo));
+
+  if (irrecv.decode(&results)) { 
+    Serial.print("irCode: ");
+    Serial.print(results.value, HEX);
+    //Serial.print(", bits: ");
+    //Serial.println(results.bits);
+    irrecv.resume();
   } 
-  delay(600); //延时600毫秒（按键消抖）
-  if(results.value == 0xFFA25D)//如果接收到的信号为0xFFA25D
-  {
-    digitalWrite(ledPin1,HIGH);//点亮LED1
+  //delay(600);
+  int leftnext = left.currentPosition();
+  int rightnext = right.currentPosition();
+  if(results.value == 0xFF18E7) { // 2, move forward
+    leftnext -= 100;
+    rightnext += 100;    
+  } else if(results.value == 0xFF4AB5) { // 8, move back
+    leftnext += 100;
+    rightnext -= 100;    
+  } else if(results.value == 0xFF10EF) { // 4, turn left
+    leftnext += 100;
+    rightnext += 100;    
+  } else if(results.value == 0xFF5AA5) { // 6, turn right
+    leftnext -= 100;
+    rightnext -= 100;    
+  } else if (results.value == 0xFF38C7) { // 5, stop
+    
   }
-  else if(results.value == 0xFFE21D)//如果接收到的信号为0xFFE21D
-  {
-    digitalWrite(ledPin2,HIGH);//点亮LED2
-  }
-  else
-  {
-    digitalWrite(ledPin1,LOW);//熄灭LED1
-    digitalWrite(ledPin2,LOW);//熄灭LED2
-  }
+  left.moveTo(leftnext);
+  right.moveTo(rightnext);
+  left.run();
+  right.run();
+
 }
